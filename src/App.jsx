827b37a5a5208ca12a81b3a3efda27a68c65674d
KalaSubramani <kalasubramani@gmail.com>
2023-12-18 21:00:45 -0500

    added try catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,60 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import axios from "axios";
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [petsData, setPetsData] = useState([])
- 
-  //connect to api
-  useEffect(()=>{
-    const fetchPets = async ()=>{
-      const {data} = await axios.get("http://localhost:3000/api/pets");
-     
-      //update use state with sorted pet data
-      setPetsData(
-                    data.sort((a, b) => {
-                      if (a.name < b.name)
-                        return -1;
-                      if (a.name > b.name)
-                        return 1;
-                      return 0;
-                    })
-                   );    
-    }
-    fetchPets();
-  },[])
+  const [petsData, setPetsData] = useState([]);
 
-  const petList= petsData?.map((pet)=>{
-     return( 
-     <div key={pet.id} className='petsDiv'>
+  //connect to api
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3000/api/pets");
+
+        //update use state with sorted pet data
+        setPetsData(
+          data.sort((a, b) => {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+          })
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPets();
+  }, []);
+
+  const petList = petsData?.map((pet) => {
+    return (
+      <div key={pet.id} className="petsDiv">
         <p>{pet.id}</p>
         {/* highlight favorite pet */}
-        <p className={pet.is_favourite?"highlight":"normal"}>{pet.name}</p>      
-     </div> 
-     );
-  })
+        <p className={pet.is_favourite ? "highlight" : "normal"}>{pet.name}</p>
+      </div>
+    );
+  });
 
   return (
     <>
-      <h1>Pet finder [{petsData.length}] - Fullstack flow</h1>
+      <h1>Pet finder [{petsData.length}]<sup className="count">Count</sup> - Fullstack flow</h1>
       {/* display error message */}
-      {petsData.length<=0? <p className='errorMessage'>There are no pets to display.</p>:""}
-      <div className='petListContainer'>
-        <p className='message'>Favorite pets are highlighted.</p>
-        <div className='petListHeader'>
-           <p>PetId</p><p>PetName</p> 
+      {petsData.length <= 0 ? (
+        <p className="errorMessage">There are no pets to display.</p>
+      ) : (
+        ""
+      )}
+      <div className="petListContainer">
+        <p className="message">Favorite pets are highlighted.</p>
+        <div className="petListHeader">
+          <p>PetId</p>
+          <p>PetName</p>
         </div>
         {petList}
       </div>
-    
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
